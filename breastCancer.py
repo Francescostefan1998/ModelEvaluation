111f7f91f116c1b4a4c49c4762e0f1920af5221c
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-02-02 18:43:56 +0100

    combining transformers and estimator in a pipeline

--- breastCancer.py
+++ breastCancer.py
@@ -13,3 +13,20 @@ y = le.fit_transform(y)
 le.classes_ 
 print("-----------------------classes with labelencoder transformation----------------------------------")
 print(le.classes_)
+le.transform(['M', 'B'])
+print("-----------------------see that the class label Malignant or benign----------------------------------")
+print(le.transform(['M', 'B']))
+# divide the dataset into training and test in 80/20
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, stratify=y, random_state=1)
+
+# combining transformers and estimators in a pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2), LogisticRegression())
+pipe_lr.fit(X_train, y_train)
+y_pred = pipe_lr.predict(X_test)
+test_acc= pipe_lr.score(X_test, y_test)
+print(f'Test accuracy: {test_acc:.3f}')
