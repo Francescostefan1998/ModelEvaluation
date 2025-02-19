41a10099170405d502581397b594e7d27b9c103e
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-02-19 22:21:01 +0100

    comparing the performance of the bagging estimator with the binaryTreeClassifier

--- bagging.py
+++ bagging.py
@@ -10,3 +10,27 @@ from sklearn.model_selection import train_test_split
 le = LabelEncoder()
 y = le.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1, stratify=y)
+
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+tree = DecisionTreeClassifier(criterion='entropy', random_state=1, max_depth=None)
+
+bag = BaggingClassifier(estimator=tree, n_estimators=500, max_samples = 1.0, max_features=1.0, bootstrap=True, bootstrap_features=False, n_jobs=1, random_state=1)
+from sklearn.metrics import accuracy_score
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print(f'Decision tree train/test accuracies {tree_train:.3f}/{tree_test:.3f}')
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print(f'Bagging train/test accuracies f {bag_train:.3f}/{bag_test:.3f}')
+
+
+
+
